97fe0475824f0485e202c8db5eb7fbf06ba8c2d1
Your Name <you@example.com>
2021-10-31 20:22:11 +0600

    aa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors())
